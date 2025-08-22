61e3be9e3fa37a886a921ec7aaf646a265d1332a
Jonathan <contactoanonimo.x@gmail.com>
2025-08-22 12:34:14 -0600

    boton Buscar

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,18 +1,48 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
+//import { useEffect } from "react";
 
 const Blog = () => {
+  let [searchParams, setSearchParams] = useSearchParams();
+
+  // useEffect(() => {
+  //   if (!searchParams.get("filter")) {
+  //     setSearchParams({ filter: "default" });
+  //   }
+  // }, [searchParams, setSearchParams]);
 
   const { data, loading, error } = useFetch("https://jsonplaceholder.typicode.com/posts");
 
   if (loading) return <p>Cargando...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
+  const handleChange = (e) => {
+    let filter = e.target.value;
+    if (filter) {
+      setSearchParams({ filter });
+    } else {
+      setSearchParams({});
+    }
+  };
+
   return (
     <div>
-      <h2>Bienvenido a la página de Blog</h2>
+      <h1>Bienvenido a la página de Blog</h1>
+      <h3>Buscar</h3>
+      <input type="text" name="" onChange={ handleChange } className="form-control"
+        value={searchParams.get("filter") || ""}
+      />
+      <br />
+      <h3>Lista de Posts</h3>
       <ul className="list-group">
-        {data.map(item => (
+        {
+          data.filter(item => {
+            let filter = searchParams.get("filter");
+            if (!filter) return true;
+            let title = item.title.toLowerCase();
+            return title.includes(filter.toLowerCase());
+          })        
+          .map(item => (
           <Link to={`/blog/${item.id}`} key={item.id} className="list-group-item">
             {item.id} - {item.title}
           </Link>
