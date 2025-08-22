9d112e12b896a233e8a76b807592525114dd49db
Jonathan <contactoanonimo.x@gmail.com>
2025-08-21 20:53:33 -0600

    detail

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,9 +1,21 @@
+import { useParams, Link } from 'react-router-dom';
+import { useFetch } from "../hooks/useFetch";
+
 const BlogDetails = () => {
-  return (
-    <div>
-      <h2>Detalles del Blog</h2>
-    </div>
-  );
+    const params = useParams();
+    console.log(params); // Aquí puedes ver el ID del blog que se pasa como parámetro
+
+    const { data, loading, error } = useFetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`);
+    if (loading) return <p>Cargando...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+
+    return (
+        <div>
+        <h2>{data.id} - {data.title}</h2>
+        <p>{data.body}</p>
+        <Link to="/blog" className="btn btn-dark">Volver al Blog</Link>
+        </div>
+    );
 }
 
 export default BlogDetails;
